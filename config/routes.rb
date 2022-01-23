51de9d73d41bd63a46f1c459551449a156f8c45a
Grits <gritsdev@gmail.com>
2022-01-23 12:52:35 -0300

    Added flash partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
   # Defines the root path route ("/")
+  get "sign_up", to: "registrations#new"
   root to: "main#index"
   # root "articles#index"
 end
